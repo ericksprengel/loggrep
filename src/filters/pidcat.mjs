8ccd0e38279dad079dfca1787d0dfbe78b52169b
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-08-09 21:13:34 -0300

    filters: add pidcat structure

--- src/filters/pidcat.mjs
+++ src/filters/pidcat.mjs
@@ -1,11 +1,41 @@
-const pidcatFilter = (line, level, tag, pid, message) => {
-  return pid === '26036'
+import { spawn } from 'child_process'
+import readline from 'readline'
+
+const APP_START = /ADD_APP_START/
+const APP_END = /ADD_APP_END/
+
+const loadPidsByPackage = () => {
+  // adb shell ps
 }
 
-const filters = [
-  pidcatFilter
-]
+export default async ({ packages = [] }) => {
+  let PIDS = loadPidsByPackages(packages)
 
-export {
-  filters,
+  const pidcatFilter = (line, level, tag, pid, message) => {
+    return PIDS.includes(pid)
+  }
+
+  const onNewLine = (line, level, tag, pid, message) => {
+    // TODO: update PIDS
+    if (APP_END.test(message)) {
+      PIDS = PIDS.filter(pidCached => pidCached !== pid)
+    } else if (APP_START.test(message)) {
+      PIDS = [
+        ...PIDS,
+        pid,
+      ]
+    }
+  }
+
+  const hooks = {
+    onNewLine,
+  }
+  const filters = [
+    pidcatFilter
+  ]
+
+  return {
+    hooks,
+    filters,
+  }
 }
