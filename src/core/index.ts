1d587ffb3a7047047f37f455d98075ffb1b61c0c
Erick Massa Sprengel <erick.sprengel@gmail.com>
2021-12-24 15:15:03 -0300

    feat(hooks): add onNewLine hook

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,12 +5,17 @@ import {start} from '../input/adb'
 import {LogEntry} from '../types/log'
 
 const main = async (filters: string[], config: object) => {
-  // 1. LOAD FILTER MODULES
+  // 1. Load filter modules
   const FILTER_MODULES = await loadFilters(config, filters)
 
-  // 2. START INPUT READER LISTENER
+  // 2. Start input reader listener
   start((entry: LogEntry): void => {
-    // 2.1 IGNORE LINE IF NO ONE WOULD SHOW IT
+    // 2.1 Process onNewLine hooks
+    for (const loggrepHandlerInstance of FILTER_MODULES) {
+      loggrepHandlerInstance.hooks?.onNewLine(entry)
+    }
+
+    // 2.2 Ignore line if no one would show it
     if (!shouldShowLogEntry(FILTER_MODULES, entry)) {
       return
     }
